db8c0987ec40aa40e4f43007a095a83fca7bb61c
pkiuna <pkkygb@mail.umkc.edu>
2020-07-30 07:19:40 -0500

    Project 2

--- Q4.py
+++ Q4.py
@@ -27,67 +27,27 @@ headlines = []
 for filename in os.listdir(directory):
     if 'Articles' in filename:
         print(filename)
+        articleData = p.read_csv(directory + filename)
+        headlines.extend(list(articleData.headlines.values))
 
+len(headlines)
+all_headlines = [h for h in all_headlines if h != "Unknown"]
+len(all_headlines)
 
+#Process Data
+def clean_data(text):
+    text = "".join(v for v in text if v not in string.punctuation).lower()
+    text = text.encode("utf8").decode("ascii",'ignore')
+    return text
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-x = 0
-
-for filename in os.listdir(currr_dir):
-    if 'Articles' in filename:
-
-        if x == 0:
-            print(filename)
-
-        article_df = pd.read_csv(currr_dir + filename)
-
-        if x == 0:
-            print(article_df.shape)
-            print(article_df.columns)
-            print(article_df.head(5))
-            print(article_df.tail(5))
-
-        all_headlines.extend(list(article_df.headline.values))
-
-        if x == 0:
-            print(article_df.headline)
-            print(article_df.headline.values)
-
-        x = 1
-
-        break
-
-all_headlines = [h for h in all_headlines if h != 'Unknown']
-print(len(all_headlines))
-print(all_headlines[:5])
-#dataset cleaning
-def clean_text(txt):
-    txt = "".join(w for w in txt if w not in string.punctuation).lower()
-    txt = txt.encode("utf8").decode("ascii","ignore")
-    return txt
-corpus = [clean_text(x) for x in all_headlines]
-print(corpus[:10])
+corpus = [clean_data(headline) for headline in all_headlines]
+corpus[:10]
 
 #tokenization
 tokenizer = Tokenizer()
 
-
 def get_sequence_of_tokens(corpus):
     q = 0
-    # tokenization
     tokenizer.fit_on_texts(corpus)
     total_words = len(tokenizer.word_index) + 1
 
@@ -97,7 +57,6 @@ def get_sequence_of_tokens(corpus):
     # convert data into sequence of tokens
     input_sequences = []
     for line in corpus:
-
         if q == 0:
             print(line)
 
@@ -106,19 +65,16 @@ def get_sequence_of_tokens(corpus):
         if q == 0:
             print(token_list)
             print(len(token_list))
-
         for i in range(1, len(token_list)):
             n_gram_sequence = token_list[:i + 1]
             if q == 0:
                 print(n_gram_sequence)
-
             input_sequences.append(n_gram_sequence)
 
             if q == 0:
                 print(input_sequences)
 
             q = 1
-
         return input_sequences, total_words
     input_sequence, total_words = get_sequence_of_tokens(corpus)
     print(total_words)
@@ -128,12 +84,11 @@ def get_sequence_of_tokens(corpus):
     input_sequences = np.array(pad_sequences(input_sequence, maxlen=max_sequence_len, padding='pre'))
     predictors, labels = input_sequences[:, :-1], input_sequences[:, -1]
     labels = ku.to_categorical(labels, num_classes=total_words)
-
-    return predictors, labels, max_sequence_len
+     return predictors, labels, max_sequence_len
 
  predictors, labels, max_seq_len = generate_padded_sequences(input_sequence)
 
- #
+ #Creating Model
 def create_model(maxSequenceLength, total_words):
     input_len = maxSequenceLength - 1
 
@@ -157,10 +112,11 @@ def create_model(maxSequenceLength, total_words):
 
 model = create_model(max_seq_len, total_words)
 model.summary()
-# fitting (training) the model by passing predictors and labels as training data
 
+# fitting (training) the model by passing predictors and labels as training data
 model.fit(predictors, labels, epochs=100, verbose=5)
-#generate the text
+
+#generate the text to predict headline
 def generate_text(seed_text, next_words, model, max_seq_len):
     w = 0
     for _ in range(next_words):
---
+++ Q5.py
@@ -0,0 +1,179 @@
+
+import numpy as np
+import matplotlib.pylot as plt
+from time import time
+from random import randint
+
+from keras.datasets import cifar_10
+
+from keras.utils import np_utils
+from keras.layers import Dropout
+from keras.callbacks import TensorBoard
+from keras.layers import Flatten
+from keras.optimizers import SGD
+from keras.contraints import maxnorm
+from keras.layers import Dense, Input
+from keras.models import Model
+
+from keras import regulizers
+from keras import backend as k
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.model import loadModel
+
+#autoencoder
+input_image = Input(shape=(2000,))
+encode = Dense(32, activation='relu')(input_img)
+output_image = Dense(3000, activation='relu'(encoded))
+#input for reconstruction
+auto_Encoder = Model(input_image, output_image)
+auto_Encoder.compile(optimizer='adadelta', loss='binary_crossentropy', metrics= ["ACC"])
+
+from keras.datasets import cifar_10
+# receive data-set
+(X_train.y_train), (x_test,y_test) = cifar_10.load.data()
+
+#normalize
+x_train = x_train
+x_test = x_test
+x_train = x_train / 255
+x_test = x_test / 255
+
+#puting data into encoder
+history = auto_Encoder.fit(x_train,x_train,
+    epochs=10,
+    batchSize=200,
+    shuffle=true,
+    validateDate=(x_test, x_test))
+
+#copying data for use in step b
+x_1train = x_train
+for a in range (0, 400000):
+    Pred = auto_Encoder.predict(x_train[a].reshape(1,2000))
+x_1test = x_copyTest()
+for j in range(0,20000):
+    Pred = auto_Encoder.predict(x_train[a].reshape(1,2000))
+    x_test[a] = Pred
+
+x_1train = np.array(x_1train).reshape([32,32,3])
+x_1test = np.array(x_1test).reshape([32,32,3])
+print(x_1train.shape)
+print(x_1test.shape)
+
+#modeling for CNN
+model = model.sequencial()
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3), kernel_contraint = maxnorm(3)))
+model.add(layers.Dropout(0.2))
+model.add(layers.Conv2D(32,(3,3), activation = 'relu',kernel_contraint=maxnorm(3)))
+model.add(layers.MaxPooling2D(poolSize=(4,4)))
+model.add(layers.Flatten())
+model.add(layers.Dense(500, activation='relu',kernel_contraint= maxnorm(3)))
+model.add(layers.Dropout(0.6))
+model.add(layers.Dense(numberClasses,activation='softmax'))
+
+#compile
+epoch = 30
+learningRate = 0.05
+
+Decay = learningRate/epoch
+s_Gradient=keras.optimizers.s_Gradient(lr=learningRate, Decay=Decay)
+model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits =True), optimizer=s_Gradient,metrics = ["ACC"])
+
+print(x_1test.shape)
+print(x_1train.shape)
+print(y_test.shape)
+print(y_train.shape)
+
+historyOne = model.fit(x_1train, y_train, validationData=(x_1test,y_test), epoch=epoch)
+
+scores = model.evaluate(x_1test,y_test,verbose=0)
+print("Scores: ", scores[1])
+
+#b
+import numpy as np
+import matplot.pyplot as ply
+from time import time
+from random import randint
+
+from keras.datasets import cifar_10
+
+import tensorFlow as tf
+from tensorflow import keras
+from tensorflow.keras import datasets,layers, models
+from tensorflow.keras.models import loadModel
+
+from keras.utils import no_utils
+from keras.contraints import maxnorm
+from keras.layers import Dense,Input
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.optimizers import SGD
+from keras.callbacks import TensorBoard
+
+from Keras import regualizers
+from keras import backend as d
+# receive data-set
+(X_train.y_train), (x_test,y_test) = cifar_10.load.data()
+
+#normalize
+x_train = x_train.astype('uint8') / 255
+x_test = x_test.astype('uint8') / 255
+
+from sklearn.decomposisition import PCA
+from sklearn.preprocessing import StandardScaler
+standard_Scaler = StandardScaler()
+pca = PCA()
+pca.fit(x_train)
+
+#applying PCA
+
+xTestPCA = pca.transform(x_test)
+xTrainPCA = pca.transform(x_train)
+xTrainPCA = x_trainPCA.reshape(32,32,3)
+xTestPCA = xTestPCA.reshape(32,32,3)
+
+#compiling model
+model = model.sequencial()
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3), kernel_contraint = maxnorm(3)))
+model.add(layers.Dropout(0.2))
+model.add(layers.Conv2D(32,(3,3), activation = 'relu',kernel_contraint=maxnorm(3)))
+model.add(layers.MaxPooling2D(poolSize=(4,4)))
+model.add(layers.Flatten())
+model.add(layers.Dense(500, activation='relu',kernel_contraint=maxnorm(3)))
+model.add(layers.Dropout(0.6))
+model.add(layers.Dense(numberClasses,activation='softmax'))
+
+epoch = 25
+learningRate = 0.05
+Decay -= learningRate /epoch
+s_Gradient=keras.optimizers.s_Gradient(lr=learningRate, Decay=Decay)
+model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits =True), optimizer=s_Gradient,metrics=["ACC"])
+
+historyTwo = model.fit(xTrainPCA, y_train, validationData=(xTestPCA,y_test), epoch=epoch)
+scores = model.evaluate(xTestPCA,y_test,verbose = 0)
+print("Scores: ",scores[1]*100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
